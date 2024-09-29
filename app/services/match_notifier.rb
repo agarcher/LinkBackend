54ddc234f3e2d6ab349e24458943764966a853f4
Adam Archer <agarcher@gmail.com>
2024-09-29 10:34:25 -0400

    include cafe url in message

--- app/services/match_notifier.rb
+++ app/services/match_notifier.rb
@@ -4,9 +4,15 @@ class MatchNotifier
   end
 
   def self.notify_users(user, matched_user)
-    message = "Great news! We've matched you with #{matched_user.first_name}. Reach out to find a time to meet this week."
-    vcard_url = Rails.application.routes.url_helpers.generate_vcard_url(user_id: matched_user.id, host: "https://#{ENV['HOSTNAME']}")
+    message = "Great news! We've matched you with #{matched_user.first_name}. Reach out to find a time to meet this week.
 
-    MessageSender.new(user.phone_number).send_message(message, [ vcard_url ])
+You'll be heading to Isle of Coffee. Grab a drink, and explore the area together!
+
+https://maps.app.goo.gl/mKu1Ji9mX8CMdjKv5"
+    media_urls = [
+      Rails.application.routes.url_helpers.generate_vcard_url(user_id: matched_user.id, host: "https://#{ENV['HOSTNAME']}"),
+    ]
+
+    MessageSender.new(user.phone_number).send_message(message, media_urls)
   end
 end
